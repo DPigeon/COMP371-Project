89fd195c776f7a0b5afee265e8f9ca44ab654806
Kevin Luu <kevinateur@hotmail.com>
2019-08-06 23:31:30 -0400

    #1-Add classe to seperate concerns

---
+++ Source/Star.cpp
@@ -0,0 +1,9 @@
+//
+//  Star.cpp
+//  COMP371_Framework
+//
+//  Created by Kevin Luu on 2019-08-06.
+//  Copyright © 2019 Concordia. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Source/Star.h
@@ -0,0 +1,13 @@
+//
+//  Star.h
+//  Area51
+//
+//  Created by Kevin Luu on 2019-08-06.
+//  Copyright © 2019 Concordia. All rights reserved.
+//
+
+#ifndef Star_h
+#define Star_h
+
+
+#endif /* Star_h */
